17f0674d13b52068f6d1871cbec28a794d7db04f
Frances <delacruzfrances0720@gmail.com>
2024-10-21 23:06:27 +0800

    feat: implemented functions in ConsoleManager

--- operatingSystem/ConsoleManager.cpp
+++ operatingSystem/ConsoleManager.cpp
@@ -1,11 +1,21 @@
 #include "ConsoleManager.h"
 
+#include <iostream>
+
 ConsoleManager* ConsoleManager::sharedInstance = nullptr;
 
 ConsoleManager::ConsoleManager()
 {
 	this->running = true;
 	this->consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	const std::shared_ptr<MainConsole> mainConsole = std::make_shared<MainConsole>();
+	const std::shared_ptr<SchedulingConsole> schedulingConsole = std::make_shared<SchedulingConsole>();
+
+	this->consoleTable["MainConsole"] = mainConsole;
+	this->consoleTable["SchedulingConsole"] = schedulingConsole;
+
+	this->switchConsole("MainConsole");
 }
 
 ConsoleManager* ConsoleManager::getInstance()
@@ -35,22 +45,50 @@ void ConsoleManager::process() const
 
 }
 
-// TODO: Implement this function
 void ConsoleManager::switchConsole(std::string consoleName)
 {
+	if (this->consoleTable.find(consoleName) != this->consoleTable.end())
+	{
+		// Key exists in the map
+		this->previousConsole = this->currentConsole;
+		this->currentConsole = this->consoleTable[consoleName];
+		this->currentConsole->onEnabled();
+	}
+	else
+	{
+		// Key does not exist in the map
+		std::cout << "Console " << consoleName << " not found.\n";
+	}
 
 }
 
-// TODO: Implement this function
-void ConsoleManager::registerScreen(std::shared_ptr<BaseScreen> console)
+void ConsoleManager::registerScreen(std::shared_ptr<BaseScreen> screenRef)
 {
-
+	if (this->consoleTable.find(screenRef->name) == this->consoleTable.end())
+	{
+		this->consoleTable[screenRef->name] = screenRef;
+	}
+	else
+	{
+		std::cout << "Screen " << screenRef->name << " already exists.\n";
+	}
 }
 
 // TODO: Implement this function
 void ConsoleManager::switchToScreen(std::string screenName)
 {
-
+	if (this->consoleTable.find(screenName) != this->consoleTable.end())
+	{
+		// Key exists in the map
+		this->previousConsole = this->currentConsole;
+		this->currentConsole = this->consoleTable[screenName];
+		this->currentConsole->onEnabled();
+	}
+	else
+	{
+		// Key does not exist in the map
+		std::cout << "Screen " << screenName << " not found.\n";
+	}
 }
 
 // TODO: Implement this function

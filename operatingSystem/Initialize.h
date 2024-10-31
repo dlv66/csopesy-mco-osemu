422ae6eae0bddd780b9112c03dccdba18c77653e
Leeroi Villamiel <denisvillamiel@gmail.com>
2024-10-31 17:09:23 +0800

    feat: Initialize function files ready

--- operatingSystem/Initialize.h
+++ operatingSystem/Initialize.h
@@ -1,2 +1,28 @@
+#pragma once
+#include <vector>
+#include "Process.h"
+#include "Core.h"
+#include <iostream>
+#include <fstream>
 
+
+class Initialize
+{
+public:
+	// Constructor
+	Initialize();
+
+	// Variables
+	std::string myText;
+	int numCPU;
+	std::string scheduler;
+	int quantumCycles;
+	int batchProcessFreq;
+	int minIns;
+	int maxIns;
+	int delayPerExec;
+
+	// Functions
+	void start();
+};
 

cd5c119c8f31d9be15d59c07b068c6684ed151fb
Frances <delacruzfrances0720@gmail.com>
2024-11-03 17:49:05 +0800

    fix: fixed printing of active processes

--- operatingSystem/GlobalScheduler.cpp
+++ operatingSystem/GlobalScheduler.cpp
@@ -145,41 +145,46 @@ void GlobalScheduler::incrementProcessID()
 void GlobalScheduler::handleScreenLS() const
 {
 
+	int nonActiveCores = 0;
 	this->getCPUUtilization();
 
 	std::cout << "\n\n\n";
 	std::cout << "---------------------------------------------------------------------------------------";
 	std::cout << "\n";
 	std::cout << "Running Processes: " << std::endl;
-	if(this->scheduler->activeProcessesList.empty())
+	for (int i = 0; i < this->scheduler->nCores; i++)
+	{
+		if (this->scheduler->coreList[i].process != nullptr)
+		{
+			std::shared_ptr<Process> runningProcess = this->scheduler->coreList[i].process;
+			std::cout << std::setw(20) << std::left << runningProcess->getName()
+				<< std::setw(40) << std::left << runningProcess->getTimestampStarted()
+				<< "Core: " << std::setw(10) << std::left << runningProcess->getCPUCoreID()
+				<< runningProcess->getCommandCounter() << "/" << runningProcess->getLinesOfCode() << std::endl;
+		} else
+		{
+			nonActiveCores++;
+		}
+	}
+
+	if (nonActiveCores == this->scheduler->nCores)
 	{
 		std::cout << "No Active Processes" << std::endl;
 	}
-	else
-	{
-		for (int i = 0; i < this->scheduler->nCores; i++)
-		{
-			if (this->scheduler->coreList[i].process != nullptr)
-			{
 
-				std::shared_ptr<Process> runningProcess = this->scheduler->coreList[i].process;
-				std::cout << std::setw(20) << std::left << runningProcess->getName()
-					<< std::setw(40) << std::left << runningProcess->getTimestampStarted()
-					<< "Core: " << std::setw(10) << std::left << runningProcess->getCPUCoreID()
-					<< runningProcess->getCommandCounter() << "/" << runningProcess->getLinesOfCode() << std::endl;
-			}
-		}
-	}
-	std::cout << "\nWaiting Processes: " << std::endl;
-	for (auto& process : this->scheduler->activeProcessesList)
+	if(!this->scheduler->activeProcessesList.empty())
 	{
-		if (process != nullptr)
+		std::cout << "\nWaiting Processes: " << std::endl;
+		for (auto& process : this->scheduler->activeProcessesList)
 		{
-			if (process->getCPUCoreID() == -1)
-				std::cout << std::setw(20) << std::left << process->getName()
-				<< std::setw(40) << std::left << process->getTimestampStarted()
-				<< "Core: " << std::setw(10) << std::left << "N/A"
-				<< process->getCommandCounter() << "/" << process->getLinesOfCode() << std::endl;
+			if (process != nullptr)
+			{
+				if (process->getCPUCoreID() == -1)
+					std::cout << std::setw(20) << std::left << process->getName()
+					<< std::setw(40) << std::left << process->getTimestampStarted()
+					<< "Core: " << std::setw(10) << std::left << "N/A"
+					<< process->getCommandCounter() << "/" << process->getLinesOfCode() << std::endl;
+			}
 		}
 	}
 
@@ -200,7 +205,7 @@ void GlobalScheduler::handleScreenLS() const
 	}
 
 	std::cout << "\n";
-	Sleep(100);
+	Sleep(200);
 }
 
 void GlobalScheduler::handleReportUtil() const
@@ -358,7 +363,7 @@ void GlobalScheduler::handleSchedulerTest(long long minIns, long long maxIns, lo
 			GlobalScheduler::getInstance()->scheduler->addProcess(process); // add process to scheduler queue
 			ConsoleManager::getInstance()->registerScreenNoCout(screen); // register screen to table of layouts/screens, NO COUT IMPORTANT.
 			this->incrementProcessID(); // increment process ID
-			Sleep(100);
+			Sleep(200);
 		}
 
 	}
